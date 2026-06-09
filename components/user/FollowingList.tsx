bf00768450211fa45aa05e84513fcafe878f023e
manoj.barnage <manojbrj111@gmail.com>
2026-06-09 14:58:44 +0530

    feat:follwer following added

---
+++ components/user/FollowingList.tsx
@@ -0,0 +1,39 @@
+'use client'
+import React, { useState } from 'react'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
+import { UserWithFollowers } from './UserProfile';
+import UserSummery from '../blog/UserSummery';
+import FollowButton from './FollowButton';
+
+function FollowingList({user}:{user:UserWithFollowers}) {
+  const [open,setOpen] = useState<boolean>(false);
+  return (
+    <div>
+      <Dialog>
+  <DialogTrigger>
+    <span>
+      {user._count.followings} Following
+    </span>
+  </DialogTrigger>
+  <DialogContent className='max-w-[500px] w-[90%]'>
+    <DialogHeader>
+      <DialogTitle>Follwings</DialogTitle>
+      
+    </DialogHeader>
+<div>
+  {user.followings.map((item)=>{
+  console.log(item)
+    return (<div key={item.following.id} className='flex gap-4 items-center justify-between'>
+    <UserSummery user={item.following}/>
+    <FollowButton user={item.following}  isFollowing={!!item.following.followers.length} isList={true}/>
+    </div>)
+  })}
+</div>
+  </DialogContent>
+</Dialog>
+  
+    </div>
+  )
+}
+
+export default FollowingList
