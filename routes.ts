77843f158fd9fdd3207ce8ccbc93e7d165923af9
manoj barange <manojbrnj111@gmail.com>
2026-07-14 17:16:17 +0530

    sitemap added to public route

--- routes.ts
+++ routes.ts
@@ -1,4 +1,4 @@
-export const publicRoutes = ["/", "/email-verification", "/password-reset-form", "/password-email-form", /^\/blog\/feed\/\d+$/, /^\/blog\/details\/[\w-]+$/,];
+export const publicRoutes = ["/","/sitemap.xml","/robots.txt", "/email-verification", "/password-reset-form", "/password-email-form", /^\/blog\/feed\/\d+$/, /^\/blog\/details\/[\w-]+$/,];
 export const authRoutes = ['/login', "/register"];
 export const apiAuthPrefix = "/api/auth";
 export const LOGIN_REDIRECT = "/blog/feed/1"
